b593ec733f53051f849fac401717759a9f881375
FATIMAEZZAHRAENNACIRY <fatimaezzahra.ennaciry@um6p.ma>
2020-12-21 16:24:45 +0100

    a comment about your changes

--- TP1/ex02/gauss.c
+++ TP1/ex02/gauss.c
@@ -2,33 +2,58 @@
 #include <stdlib.h>
 #define N 10
 
-float *remontee(float A[N][N], float B[N], int n)
+double *remontee(double A[N][N], double B[N], int n)
 {
 
   /******Implement the solution here******/
-  /***************************************/
+  double *x;
+  x= malloc(sizeof(double)*n);
+  double s;
+  x[n-1]=B[n-1]/A[n-1][n-1];
+  for (int i=n-2; i>=0; i--)
+  {
+	  s=0;
+	  for (int j=i+1; j<=n-1;j++)
+		  s+=A[i][j]*x[j];
+	  x[i]=(B[i]-s)/A[i][i];
+  }
+
+		   
   
-  /***************************************/
   return x;
 }
 
-float *gauss(float A[N][N] ,float B[N], int n)
+double *gauss(double A[N][N] ,double B[N], int n)
 {
   /******Implement the Gaussian elimination******/
   /**********************************************/
-  
+  double *x;
+  x=malloc(sizeof(double)*n);
+  double f;
+  for(int k=0;k<n-1;k++)
+  {
+	  for(int i=k+1;i<n;i++)
+	  {
+		  f=A[i][k]/A[k][k];
+		  for(int j=0;j<n;j++)
+		  {
+			  A[i][j]=A[i][j]-f*A[k][j];
+		  }
+		  B[i]=B[i]-f*B[k];
+	  }
 
-  /**********************************************/
-  /*Resolve the system using the back substitution method*/
-  /*You can use your solution of the exercice 1-b*/
+  }
+
+	  
+
   x = remontee(A, B, n);
   return(x);
 }
 
 int main()
 {
-  float   A[N][N], B[N];
-  float   *x;
+  double   A[N][N], B[N];
+  double  *x;
   int     n;
 
   printf("Enter the size of the matrix: ");
@@ -41,7 +66,7 @@ int main()
     for(int j = 0; j < n; j++)
     {
       printf("A[%d][%d] = ", i, j);
-      scanf("%f", &A[i][j]);
+      scanf("%lf", &A[i][j]);
     }
   }
 
@@ -50,7 +75,7 @@ int main()
   for (int i = 0; i < n; i++)
   {
     printf("B[%d] = ", i);
-    scanf("%f", &B[i]);
+    scanf("%lf", &B[i]);
   }
 
   /* The calculation of the result */
@@ -59,6 +84,6 @@ int main()
   /* Printing the results */
   printf("\nThe resulting vector: [");
   for (int i = 0; i < n; i++)
-    printf("%f%c", x[i], ",]"[i == n - 1]);
+    printf("%lf%c", x[i], ",]"[i == n - 1]);
 }
 
