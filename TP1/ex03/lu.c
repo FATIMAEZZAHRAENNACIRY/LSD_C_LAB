6c63b7a5db38acf604acaf9c4b3965488165c089
FATIMAEZZAHRAENNACIRY <fatimaezzahra.ennaciry@um6p.ma>
2020-12-22 20:53:58 +0100

    a comment about your changes

--- TP1/ex03/lu.c
+++ TP1/ex03/lu.c
@@ -3,13 +3,51 @@
 #define N 10
 
 float *lu(float A[N][N], float B[N], int n)
+
 {
+       float *x;
+       x=malloc(sizeof(float) * n);
 
-  /******Implement the solution here******/
-  /***************************************/
+       float l[N][N],y[N];
+       float s;
+       
+       for(int k=0;k<n-1;k++)
+       {
+	       for (int i=k+1;i<n;i++)
+	       {
+		       l[i][k]=A[i][k]/A[k][k];
+		       for (int j=k+1;j<n;j++)
+		       
+		       {
+			       A[i][j]=A[i][j]-l[i][k]*A[k][j];
+		       } 
+	       }
+       }
+       y[0]=B[0];
+       for(int i=1;i<n;i++)
+       {
+	       s=0;
+	       for(int j=0; j<=i-1; j++)
+	       {
+		       s=s+l[i][j]*y[j];
+	       }
+	       y[i]=B[i]-s;
+       }
+       x[n-1]=y[n-1]/A[n-1][n-1];
+
+       for (int i=n-2;i>=0;i--)
+       {
+	       s=0;
+	       for (int j=i+1;j<n;j++)
+	       {
+		       s=s+A[i][j]*x[j];
+	       }
+	       x[i]=(y[i]-s)/A[i][i];
+       }
+ 
 
 
-  /***************************************/
+  
   return (x);
 }
 
@@ -29,7 +67,7 @@ int main()
     for(int j = 0; j < n; j++)
     {
       printf("A[%d][%d] = ", i, j);
-      scanf("%f", &A[i][j]);
+      scanf("%fi", &A[i][j]);
     }
   }
 
